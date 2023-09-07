5a4533de86e363ebc97e4de147f0946faa0d764b
atika <atikamohammad8@gmail.com>
2023-09-07 07:48:00 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  main - A C program that prints aline to the standard error
- *   Return 1 (Success)
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 return (1);
 }
