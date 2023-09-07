1b20d296be926be7a1cc898e4c4a7bd5d2520727
atika <atikamohammad8@gmail.com>
2023-09-07 07:03:12 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * Description: print a qoute ueing write function
  *    ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: Always 1 ( not success).
+ * Return: Always 1 (error).
 */
 
 int main(void)
