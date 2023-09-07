adfe5aab7f0637d15652c45cf8b7cf59ac7ee0bf
atika <atikamohammad8@gmail.com>
2023-09-07 06:37:58 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program thatb prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
