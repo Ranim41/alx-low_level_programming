f132720be818658fa0bed1257053319c20318494
atika <atikamohammad8@gmail.com>
2023-09-07 07:26:44 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Description: print a qoute ueing write function
- *    ssize_t write(int fd, const void *buf, size_t count);
- *
- * Return: Always 1 (error).
-*/
-
+ *  main - A C program that prints aline to the standard error
+ *   Return 1 (Success)
+ */
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(1, quo, 59);
-	return (1);
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+return (1);
 }
