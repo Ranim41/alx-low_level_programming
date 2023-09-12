b911c71d43d7e36fb5783dd2c1c8f7f1c1f01c9d
atika <atikamohammad8@gmail.com>
2023-09-12 12:17:05 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	char al;
+
+	al = 'a';
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+	_putchar('\n');
+}
