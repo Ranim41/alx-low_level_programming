087880f91905619c0dac1f7c4fa6e23a72ffd642
atika <atikamohammad8@gmail.com>
2023-09-12 18:00:55 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - print time
+ *
+ *Description: print the time in min. for 24 hours clock
+ *
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int print_last_digit(int);
 int print_last_digit(int);
+void jack_bauer(void);
