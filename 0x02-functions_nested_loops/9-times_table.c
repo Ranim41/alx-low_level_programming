2881ed85a180fc511ce35748f1834d21f49a829e
atika <atikamohammad8@gmail.com>
2023-09-12 18:25:39 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - a function that prints the 9
+ * time table
+ *
+ * Description: it prints it followed by , and 2 spaces
+ *
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 0; j <= 9; j++)
+		{
+			k = (i * j);
+
+			if ((k / 10) > 0)
+			{
+			_putchar((k / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+				_putchar((k % 10) + '0');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int print_sign(int n);
 int print_last_digit(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
