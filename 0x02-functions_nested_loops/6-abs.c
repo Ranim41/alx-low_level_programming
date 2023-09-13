beb4680924b75a24936fa95df15d6550b276f1dc
atika <atikamohammad8@gmail.com>
2023-09-12 17:21:41 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an int
+ *@r: the value
+ *Description: The standaed libarary provides asimilar funct..
+ *
+ * Return: (0)
+ *
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+
+		return (r);
+	else
+		return (r * -1);
+}
