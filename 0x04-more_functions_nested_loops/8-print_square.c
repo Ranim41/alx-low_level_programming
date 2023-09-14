0e2fe782bcdd9cecfd9fc05b2fbad597678cd76c
atika <atikamohammad8@gmail.com>
2023-09-14 06:27:57 -0700

    0x04-more functions_nested_loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,20 +3,26 @@
  * print_squar - print a squar using the character #
  *
  * @size: is the size of the square
- * Description: print a squar
- * Return: Always 0 (success)
+ *
+ * Description: This function prints a square with the given size
+ * if size is 0 or negative, it prints a newline
  */
 
 void print_square(int size)
 {
-	int row, column;
+	int i, j;
 
 	if (size <= 0)
-                _putchar('\n');
-	for (row = 1; row <= size; row++)
+		_putchar('\n')
+	else
 	{
-		for (column = 1; column <= size; column++)
-			_putchar('#');
-		_putchar('\n');
-	}	
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+				_putchar('\n');
+			}
+		}
+	}
 }
