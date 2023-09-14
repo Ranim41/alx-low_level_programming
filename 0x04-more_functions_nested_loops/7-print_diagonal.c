88baeaf03417324c5f7d882e67b11d8c01fa14fd
atika <atikamohammad8@gmail.com>
2023-09-14 04:37:01 -0700

    0x04-more functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,21 +9,18 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
-
-	for (i = 0; i < n; i++)
-	{
-		if (n > 1)
-			for (j = 1; j < i; j++)
-			{
-				_putchar(' ');
-			}
-		_putchar('\\');
-		_putchar('\n');
-	}
+	int postn, space;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
+		}
 	}
 }
