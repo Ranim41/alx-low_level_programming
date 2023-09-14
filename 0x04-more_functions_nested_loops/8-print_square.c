9ebc976874fd28665f321112d9efac6e598ee8bf
atika <atikamohammad8@gmail.com>
2023-09-14 12:10:35 -0700

    0x04-more functions_nested_loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,20 +6,21 @@
 
 void print_square(int size)
 {
-	int i, j;
+	int row, coulmn;
 
 	if (size <= 0)
 	{
 	_putchar('\n');
-	return;
 	}
+	else
+	{
+	for (row = 0; row < size; row++)
+	{
+		for (coulmn = 0; coulmn < size; coulmn++)
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-	{
 			_putchar('#');
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 	}
 }
