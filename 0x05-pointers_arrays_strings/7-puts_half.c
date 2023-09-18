7cdb7110a8bd83a104cfb4a8414175f22a7b57ac
atika <atikamohammad8@gmail.com>
2023-09-18 10:09:14 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ *
+ * @str: input
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int full_string, half_string;
+
+	full_string = 0;
+	while (str[full_string] != '\0')
+		full_string++;
+	half_string = full_string / 2;
+
+	if (full_string % 2 == 1)
+		half_string++;
+	while (half_string < full_string)
+	{
+		_putchar(str[half_string]);
+		half_string++;
+	}
+	_putchar('\n');
+}
