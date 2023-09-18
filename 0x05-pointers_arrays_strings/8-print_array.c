1170c85b238f59b63e67b2579c117cb5c6881fc1
atika <atikamohammad8@gmail.com>
2023-09-18 10:27:51 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array -  prints n elements of an array of integers,
+ *		followed by a new line
+ *
+ * @a: first input
+ * @n: second input
+ *
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i !=(n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
