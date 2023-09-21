9e99c629b667cc41f98602987af71824eee7fad3
atika <atikamohammad8@gmail.com>
2023-09-21 06:50:59 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @s: pointer to string
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char c[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
