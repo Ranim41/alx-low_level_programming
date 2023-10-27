9bea0f6eae2aa3b3e1e587543692f9eeae3ee77e
atika <atikamohamad8@gmail.com>
2023-10-27 16:02:35 +0300

    argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - function - prints - a program that adds positive numbers
+ *
+ * @argc: number of commande line arguments
+ * @argv: an array of string containing the program command line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j,  add = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 int _putchar(char c);
 int main(int argc, char *argv[]);
 
