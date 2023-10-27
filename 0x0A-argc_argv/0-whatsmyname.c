2204aff5c1bda7023109830267d4a77411360ba9
atika <atikamohamad8@gmail.com>
2023-10-27 15:18:59 +0300

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function - prints the name of program, followed by a new line.
+ *
+ * @argc: the number of commande line arguments
+ * @argv: the array containing the program commande line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("the programe name is %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this my readme file for the task of argc and argv
