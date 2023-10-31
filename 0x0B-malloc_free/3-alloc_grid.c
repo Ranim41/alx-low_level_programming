b193514bba765076cea97ac8135a338014ba1f93
atika <atikamohamad8@gmail.com>
2023-10-31 13:43:30 +0300

    malloc free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,11 +16,15 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-		return (0);
+	{
+		return (NULL);
+	}
 
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
