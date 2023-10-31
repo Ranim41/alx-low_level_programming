bba389d44dd3407a2dd90ca10225bd2737d06839
atika <atikamohamad8@gmail.com>
2023-10-31 11:43:19 +0300

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 	size_t i;
 	char *p;
 
-	if (s1 == NULL)
+	if (*s1 == '\0')
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+	else if (*s2 == '\0')
 	{
 		s2 = "";
 	}
