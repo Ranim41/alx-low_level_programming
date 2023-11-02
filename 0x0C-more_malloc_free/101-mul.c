9782f617b13ccc90bd3e82317bb31f60860919c6
atika <atikamohamad8@gmail.com>
2023-11-02 22:06:24 +0300

    more malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -69,28 +69,32 @@ int main(int argc, char *argv[])
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-	if (!result)
-	return (1);
+		if (!result)
+			return (1);
 	for (i = 0; i <= len1 + len2; i++)
-	result[i] = 0;
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
-	digit1 = s1[len1] - '0';
-	carry = 0;
-	for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-	digit2 = s2[len2] - '0';
-	carry += result[len1 + len2 + 1] + (digit1 * digit2);
-	result[len1 + len2 + 1] = carry % 10;
-	carry /= 10;
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		digit2 = s2[len2] - '0';
+		carry += result[len1 + len2 + 1] + (digit1 * digit2);
+		result[len1 + len2 + 1] = carry % 10;
+		carry /= 10;
 	if (carry > 0)
 	result[len1 + len2 + 1] += carry;
+	}
 	for (i = 0; i < len - 1; i++)
-	if (result[i])
-	a = 1;
-	if (a)
-	_putchar(result[i] + '0');
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
 	if (!a)
-	_putchar('0');
+		_putchar('0');
 	_putchar('\n');
 	free(result);
-	return (0);
+		return (0);
 }
