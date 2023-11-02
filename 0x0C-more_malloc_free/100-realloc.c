13ba3d8b946905768a7801dca8273581f0e57c63
atika <atikamohamad8@gmail.com>
2023-11-02 16:35:58 +0300

    more malloc free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,25 +21,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		pt = malloc(new_size);
 		return (pt);
 	}
-	else if (new_size == 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else if (new_size == old_size)
-	{
-		return (ptr);
-	}
-
+	if (new_size == old_size)
+	return (ptr);
 	pt = malloc(new_size);
 	if (pt == NULL)
-	{
-		return (NULL);
-	}
+	return (NULL);
 	if (new_size > old_size)
-	{
-		new_size = old_size;
-	}
+	new_size = old_size;
 	for (i = 0; i < new_size; i++)
 	{
 		pt[i] = oldptr[i];
