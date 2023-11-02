1afee367a26859701a20e422b0f5ebac8dbaa3d7
atika <atikamohamad8@gmail.com>
2023-11-02 16:42:56 +0300

    more malloc free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,18 +15,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	size_t i;
 
 	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-
+	return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
-		return (NULL);
-	}
+	return (NULL);
 	for (i = 0; i < nmemb * size; i++)
-	{
-		ptr[i] = 0;
-	}
+	ptr[i] = 0;
 	return (ptr);
 }
