cc5c90e9f955b685b662b694cd990e422d231b09
atika <atikamohamad8@gmail.com>
2023-11-02 16:51:41 +0300

    more malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,7 @@
 
 /**
  * malloc_checked - function that allocates memory using malloc
- *
  * @b: number of integers to be allocated
- *
  * Return: void
  */
 
@@ -13,10 +11,7 @@ void *malloc_checked(unsigned int b)
 	void *ptr;
 
 	ptr = malloc(b);
-
 	if (ptr == NULL)
-	{
-		exit(98);
-	}
+	exit(98);
 	return (ptr);
 }
