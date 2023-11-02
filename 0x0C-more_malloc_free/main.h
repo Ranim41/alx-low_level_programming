0e761b3f5508f117b1b436cede33b68eefea9fea
atika <atikamohamad8@gmail.com>
2023-11-02 22:11:25 +0300

    more malloc free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int main(int argc, char *argv[]);
+int is_digit(char *s);
+void error();
+int _strlen(char *s);
 
 #endif
