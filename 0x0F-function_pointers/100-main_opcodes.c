f5808bd95000caea634a141be76f63ab397b4a99
atika <atikamohamad8@gmail.com>
2023-11-08 14:05:27 +0300

    0x0F-function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,4 +10,30 @@
 
 int main(int argc, char *argv[])
 {
+	int number_of_bytes, i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	unsigned char *ptr = (unsigned char *)main;
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%2x", ptr[i]);
+	}
+	printf("\n");
+	return (0);
+}
+
+
+
 
