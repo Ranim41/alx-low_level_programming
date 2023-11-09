f93e4904cfec705b26add0ad1d1e211a7d43e564
atika <atikamohamad8@gmail.com>
2023-11-09 13:40:36 +0300

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,10 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(num, n);
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(num,  unsigned int);
