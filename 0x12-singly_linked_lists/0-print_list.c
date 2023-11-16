29841a66dd78ca2cd2fa591695d2bc16c8daeb20
atika <atikamohammad8@gmail.com>
2023-11-16 20:08:30 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: head pointer
+ * Return: the number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+my readme file for taske of singly_linked_lists
