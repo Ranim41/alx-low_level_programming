37c497a8c9c0c218652fb4b5210395ffec22a3a7
atika <atikamohammad8@gmail.com>
2023-11-19 10:20:17 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
-void msg(void)__attribute__((constructor));
-
 /**
- * msg - prints a specific message
+ * print_befor_main - function print befor main execution
  *
- * Return: No return
+ * Return: Nothing
  */
-void msg(void)
+void __attribute__((constructor)) print_befor_main(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
 }
