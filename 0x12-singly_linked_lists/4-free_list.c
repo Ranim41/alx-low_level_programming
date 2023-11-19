6a41f84fc2d02d584d108397211b15b2028bbf97
atika <atikamohammad8@gmail.com>
2023-11-19 10:15:27 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,13 @@
 
 void free_list(list_t *head)
 {
+	list_t *new;
 
-	if (head == NULL)
-		return;
-	free_list(head->next);
-	free(head->str);
-	free(head);
+	while (head != NULL)
+	{
+		new = head->next;
+		free(head->str);
+		free(head);
+		head = new;
+	}
 }
