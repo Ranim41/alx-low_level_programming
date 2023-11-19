98953c1bf31b3f4eab7c7911febe0503545ffec4
atika <atikamohammad8@gmail.com>
2023-11-19 10:22:05 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__((constructor)) print_befor_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
