afd5dbe83b2808d27eb9473b5eb1ab76b261c9f0
atika <atikamohammad8@gmail.com>
2023-11-20 18:59:10 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,20 @@
-#include "task/print_task"
+#include "lists.h"
+
+/**
+ * print_listint- function print all the elements of the list
+ * @h: head pointer
+ * Return: size_t value
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
