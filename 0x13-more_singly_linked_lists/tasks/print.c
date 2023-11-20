b1196012afb9e3a115ffab653252bacf9c8dd04e
atika <atikamohammad8@gmail.com>
2023-11-20 19:36:50 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/tasks/print.c
+++ 0x13-more_singly_linked_lists/tasks/print.c
@@ -8,15 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-    size_t = 0;
+	size_t = 0;
 
-    while (h != NULL)
-    {
+	while (h != NULL)
+	{
         printf("%i\n", h->n);
         h = h->next;
         i++;
-    }
+	}
     return (i);
-
 }
 ~                     
