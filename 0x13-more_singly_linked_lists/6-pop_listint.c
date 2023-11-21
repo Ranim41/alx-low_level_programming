5afaaa72731db80aaafa0f7e348f9fcf54b7f71d
atika <atikamohammad8@gmail.com>
2023-11-21 09:57:53 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,9 +13,9 @@ int pop_listint(listint_t **head)
 
 	if (head == NULL)
 		return (0);
-	num = *head->n;
-	temp = *head->next;
-	free(*head);
-	*head = temp;
+	num = head->n;
+	temp = head->next;
+	free(head);
+	head = temp;
 	return (num);
 }
