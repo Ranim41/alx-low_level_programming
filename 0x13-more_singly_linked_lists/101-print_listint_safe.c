dba82028b8b186af412a9c2ecc66c8cfd4d98bf7
atika <atikamohammad8@gmail.com>
2023-11-21 13:59:39 +0300

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe- function that prints a listint_t linked list
+ * @head: head pointer
+ * Return: unsigned integer
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+	const listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		i++;
+		if (temp <= temp->next)
+		{
+			printf("->[%p] %d\n", (void *)temp->next, temp->next->n);
+			exit(98);
+		}
+		temp = temp->next;
+	}
+	return (i);
+}
