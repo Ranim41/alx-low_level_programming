4b62269024f42974e88e127bf734df58850ac26a
atika <atikamohammad8@gmail.com>
2023-11-21 09:24:04 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (temp == NULL)
 		return (NULL);
 	temp->n = n;
-	temp->next = head;
-	head = temp;
+	temp->next = *head;
+	*head = temp;
 	return (temp);
 }
