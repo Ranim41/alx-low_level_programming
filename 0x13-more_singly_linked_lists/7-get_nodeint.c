8b2bbd4c91b93abb194ac1d90decb9389196ac15
atika <atikamohammad8@gmail.com>
2023-11-21 10:18:53 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	size_t i;
 	listint_t *temp;
 
-	temp = head;
 	if (head == NULL)
 		return (0);
+	temp = head;
 	for (i = 0; i < index; i++)
+	{
 		temp = temp->next;
+	}
 	return (temp);
 }
 
