ca85f9544285bc40e37b481e0c25690c4895510d
atika <atikamohammad8@gmail.com>
2023-11-21 13:11:15 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,6 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (!temp || !temp->next)
 			return (-1);
 		temp = temp->next;
+		i++;
 	}
 	temp2 = temp->next;
 	temp->next = temp2->next;
