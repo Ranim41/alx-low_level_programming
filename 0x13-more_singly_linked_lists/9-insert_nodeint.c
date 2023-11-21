ce757954585781f104e4695a8603cc5f9747cb9d
atika <atikamohammad8@gmail.com>
2023-11-21 11:52:25 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	size_t i;
 
 	temp = malloc(sizeof(listint_t));
-	if (!temp || !head)
+	if (!temp)
 		return (NULL);
 
 	temp->n = n;
