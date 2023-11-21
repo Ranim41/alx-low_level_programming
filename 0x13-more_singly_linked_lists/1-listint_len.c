03bdac5bf508b4e52b1ed583f8d1cb4c066b5278
atika <atikamohammad8@gmail.com>
2023-11-21 09:18:20 +0300

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len- returns the number of elements in a list
+ * @h: head pointer
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
