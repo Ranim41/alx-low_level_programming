6ad0323566aad1f4277a9c75b882bef4b344e33c
atika <atikamohammad8@gmail.com>
2023-11-21 10:15:32 +0300

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index- returns the nth node of a list
+ * @head: head pointer
+ * @index: the position of nodes
+ * Return: struct
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t i;
+	listint_t *temp;
+
+	temp = head;
+	if (head == NULL)
+		return (0);
+	for (i = 0; i < index; i++)
+		temp = temp->next;
+	return (temp);
+}
+
+
