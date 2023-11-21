41729ee896f52cc2566d00bbf4ed01bd3cefeec4
atika <atikamohammad8@gmail.com>
2023-11-21 09:45:11 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head == NULL)
+	if (*head)
 	{
-		temp->next = *head;
-		*head = temp;
-		free(temp);
+		while (head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
 }
