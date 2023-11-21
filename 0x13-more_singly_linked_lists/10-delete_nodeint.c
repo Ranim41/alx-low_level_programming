264effe0898ee11dca0e3e763d9d2ff5d9436c29
atika <atikamohammad8@gmail.com>
2023-11-21 13:31:07 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *temp2;
-	size_t i = 0;
+	size_t i;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
@@ -21,12 +21,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	while (i < index - 1)
+	for (i = 0; (temp || temp->next) && (i < index - 1); i++)
 	{
-		if (!temp || !temp->next)
-			return (-1);
 		temp = temp->next;
-		i++;
 	}
 	temp2 = temp->next;
 	temp->next = temp2->next;
