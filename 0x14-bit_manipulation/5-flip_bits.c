f832ca8e0efde9695bce003686b23a822dba300b
atika <atikamohammad8@gmail.com>
2023-11-23 18:59:32 +0300

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits- returns the number of bits you would need to flip
+ *	to get from one number to another
+ * @n: int value
+ * @m: variable
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int ranim;
+	int i = 0;
+
+	renim = n ^ m;
+	while (ranim > 0)
+	{
+		i += ranim & 1;
+		ranim >>= 1;
+	}
+	return (i);
+}
