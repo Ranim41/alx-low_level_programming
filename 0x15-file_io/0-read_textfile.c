9d9b65ac79fb2d766cd99ea1cc88d6e959d7094f
atika <atikamohammad8@gmail.com>
2023-11-27 11:42:40 +0300

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL || !letters)
 		return (0);
-	fd = open("filename", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	rw = read(fd, &buff[0], letters);
